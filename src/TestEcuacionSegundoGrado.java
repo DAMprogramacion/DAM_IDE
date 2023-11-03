d493c7de76bbd538fd8a344260fee064d80090b0
manuel <mmolmil158@g.educaand.es>
2023-11-03 10:13:18 +0100

    Inicio de trabajo con IntelliJ

---
+++ src/TestEcuacionSegundoGrado.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TestEcuacionSegundoGrado {
+
+    public static void main(String[] args) {
+        //LECTURA DE LOS COEFICIENTES
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el coeficiente a:");
+        double a = sc.nextDouble();
+        System.out.println("Introduce el coeficiente b:");
+        double b = sc.nextDouble();
+        System.out.println("Introduce el coeficiente v:");
+        double c = sc.nextDouble();
+        sc.close();
+
+        //COMPROBACIÓN SI ES RESOLUBLE O NO
+        if (EcuacionSegundoGrado.esResoluble(a, b, c)) {
+            System.out.println("Es resoluble");
+            double x1 = EcuacionSegundoGrado.calcularX1(a, b, c);
+            double x2 = EcuacionSegundoGrado.calcularX2(a, b, c);
+            System.out.printf("X1 = %.3f%n", x1);
+            System.out.printf("X2 = %.3f%n", x2);
+        } else {
+            System.out.println("No es resoluble");
+        }
+    }
+}
